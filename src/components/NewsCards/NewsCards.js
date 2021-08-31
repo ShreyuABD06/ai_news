afdb90dfcce882d6584a152c8ccfa518829ea62f
ShreyuABD06 <shreyasc.997@gmail.com>
2021-08-31 10:41:16 +0530

    InfoCard Changes

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -6,8 +6,8 @@ import useStyles from './styles.js';
 const infoCards = [
     { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
     { color: '#1565c0', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
-    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5' },
-    { color: '#283593', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from BBC News' },
+    { color: '#4527a0', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, IPL...', text: 'What\'s up with PlayStation 5' },
+    { color: '#283593', title: 'News by Sources', info: 'BBC News, Google News, The Times Of India, Buzzfeed, ABC News, The Hindu...', text: 'Give me the news from BBC News' },
   ];
 
 const NewsCards = ({articles,activeArticle}) => {
